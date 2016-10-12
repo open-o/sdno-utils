199cda80ed52f101426f7e840ccac3e05c59c0f8
openo-lihua <uranus.lihua@huawei.com>
2016-10-12 22:31:59 +0800

    remove invalid error codes
    remove invalid error codes in frame
    
    SDNO-340
    
    Change-Id: Ib14091a9c09593ad2e5fc428d7ae458a2d550563
    Signed-off-by: openo-lihua <uranus.lihua@huawei.com>
    (cherry picked from commit 6b784b4ba706a1daf02c8edf8beadf908d6f67a6)

--- cbb/base/util/org.openo.sdno.frame/src/main/java/org/openo/sdno/exception/ErrorCode.java
+++ cbb/base/util/org.openo.sdno.frame/src/main/java/org/openo/sdno/exception/ErrorCode.java
@@ -24,69 +24,10 @@ package org.openo.sdno.exception;
  */
 public class ErrorCode {
 
+    /**
+     * Successful.
+     */
     public static final int SUCCESS = 0;
-
-    /**
-     * Encryption failed.
-     */
-    public static final int ENCRYPTION_FAILED = 10010001;
-
-    /**
-     * Integrity verification failed.
-     */
-    public static final int NETMATRIX_ERROR_INV_VERIFY_INTEGRITY = 10010002;
-
-    /**
-     * Query parameters legitimacy failed.
-     */
-    public static final int NETMATRIX_ERROR_INV_VERIFY_VALIDITY = 10010003;
-
-    /**
-     * Not unique exception.
-     */
-    public static final int NETMATRIX_ERROR_INV_VERIFY_UNIQUE = 10010004;
-
-    /**
-     * Resource type has not been registered,access to configuration property item has
-     * failed.
-     */
-    public static final int RESOURCE_OBTAIN_ATTR_CONFIGURATION_FAILED = 10010008;
-
-    /**
-     * Properties of the resource has not been registered, access to configuration property item has
-     * failed.
-     */
-    public static final int ATTR_OBTAIN_ATTR_CONFIGURATION_FAILED = 10010009;
-
-    /**
-     * The resource type has not been registered, failed accessing attribute validation rule.
-     */
-    public static final int RESOURCE_OBTAIN_ATTR_RULE_FAILED = 10010010;
-
-    /**
-     * The property type has not been registered, failed accessing attribute validation rule.
-     */
-    public static final int ATTR_OBTAIN_ATTR_RULE_FAILED = 10010011;
-
-    /**
-     * Resource does not exist.
-     */
-    public static final int RESOURCE_NOT_EXIST = 10010013;
-
-    /**
-     * Delete resource failure error.
-     */
-    public static final int DELETE_RESOURCE_ERROR = 10010014;
-
-    /**
-     * Invalid type or version.
-     */
-    public static final int INVALID_TYPE_OR_VERSIONI = 10010015;
-
-    /**
-     * Partially succeeded.
-     */
-    public static final int OPERATION_PART_SUCCESS = 1031001;
 
     /**
      * Successful operation.
@@ -94,14 +35,14 @@ public class ErrorCode {
     public static final int OPERATION_SUCCESS = 0;
 
     /**
+     * Partially succeeded.
+     */
+    public static final int OPERATION_PART_SUCCESS = 1031001;
+
+    /**
      * operation failed.
      */
     public static final int OPERATION_FAIL = 1031003;
-
-    /**
-     * Database operation failed.
-     */
-    public static final int COMM_ABNORMAL_ERROR = 1032001;
 
     /**
      * NE response error.
@@ -109,69 +50,9 @@ public class ErrorCode {
     public static final int DB_OPERATION_ERROR = 1032002;
 
     /**
-     * Erroneous NE response.
-     */
-    public static final int NE_RETURN_ERROR = 1032003;
-
-    /**
-     * Invalid parameter.
-     */
-    public static final int TE_ERR_INVALID_PARAMETER = 1032004;
-
-    /**
      * Internal error.
      */
     public static final int INNER_ERROR = 1032005;
-
-    /**
-     * Resource is locked.
-     */
-    public static final int RESOURCE_BELOCKED_ERROR = 1032007;
-
-    /**
-     * Resources are occupied.
-     */
-    public static final int RESOURCE_BEUESD_ERROR = 1032008;
-
-    /**
-     * Internal service failed to respond.
-     */
-    public static final int INNER_SVC_RESPONSE_ERROR = 1032025;
-
-    /**
-     * No corresponding device plug-ins found.
-     */
-    public static final int ADAPTER_NO_DEVICE_PLUGIN = 1032026;
-
-    /**
-     * Query service failure.
-     */
-    public static final int QUERY_INV_SERVICE_FAILED = 1032027;
-
-    /**
-     * Get template file failed.
-     */
-    public static final int GET_TEMPLATE_FILE_FAILED = 1032028;
-
-    /**
-     * Convert file template failed.
-     */
-    public static final int TRANS_FILE_TO_TEMPLATE_FAILED = 1032029;
-
-    /**
-     * Template mapping failed.
-     */
-    public static final int TEMPLATE_MAPPING_FAILED = 1032030;
-
-    /**
-     * Data conversion failed.
-     */
-    public static final int RESPONSE_TO_TEMPLATE_PARAMS_FAILED = 1032031;
-
-    /**
-     * Script execution failed.
-     */
-    public static final int EXEC_SCRIPT_FAILED = 1032032;
 
     /**
      * Constructor.<br>
